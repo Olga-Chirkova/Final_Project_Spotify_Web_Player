9352308f80ee1e00af5a42d7ce94c34a151021f1
Olga Chirkova <119545640+Olga-Chirkova@users.noreply.github.com>
2024-06-14 11:26:42 -0500

    Add files via upload

---
+++ 02_Selenium_WebDriver/02_BrowserStack/my_key.py
@@ -0,0 +1,3 @@
+key = 'https://BROWSERSTACK_USERNAME:BROWSERSTACK_ACCESS_KEY@hub-cloud.browserstack.com/wd/hub'
+BROWSERSTACK_USERNAME = ""   #enter your username
+BROWSERSTACK_ACCESS_KEY = ""   #enter your key
